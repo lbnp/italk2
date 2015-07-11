022fc6b756fb789e19b558f0f4c992c53730c8f4
Jun Kamoshima <kamoshima@gmail.com>
2015-07-12 07:28:09 +0900

    Initial Commit

---
+++ italk2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  italk2
+//
+//  Created by 鴨島 潤 on 2015/07/12.
+//  Copyright (c) 2015年 lbnp. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ italk2Tests/italk2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  italk2Tests.swift
+//  italk2Tests
+//
+//  Created by 鴨島 潤 on 2015/07/12.
+//  Copyright (c) 2015年 lbnp. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class italk2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
