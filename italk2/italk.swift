ab6e64362181c131fae41f8da36f1cc73bb6da3b
Jun Kamoshima <kamoshima@gmail.com>
2015-07-14 06:08:53 +0900

    implementing networking facilities

--- italk2/italk.swift
+++ italk2/italk.swift
@@ -9,8 +9,8 @@
 import Foundation
 
 class Italk {
-    var sockHandle: NSFileHandle
-    var remainData: NSData
+    var sockHandle: NSFileHandle?
+    var remainData: NSData?
     var isConnected: Bool
     var isLoggedIn: Bool
     //@IBOutlet var theController:
@@ -21,25 +21,50 @@ class Italk {
     }
 
     func connect(serverName: NSString, port: Int) {
-
+        if isConnected {
+            disconnect()
+        }
+        
+        //var hostName = CFHOst
     }
 
     func disconnect() {
+        if isLoggedIn {
+            let quitString = "/q\n"
+            let quitData = quitString.dataUsingEncoding(NSISO2022JPStringEncoding)
+            sockHandle?.writeData(quitData!)
+            isLoggedIn = false
+        }
+        
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+        sockHandle?.closeFile()
+        isConnected = false
     }
     
     func receiveMessage(notification: NSNotification) {
-    
+        
     }
     
     func sendMessage(message: NSString) {
-        
+        let messageData = message.dataUsingEncoding(NSISO2022JPStringEncoding, allowLossyConversion: true)
+        sockHandle?.writeData(messageData!)
     }
     
     func sendLine(message: NSString) {
-        
+        let toSend = message.stringByAppendingString("\n")
+        sendMessage(toSend)
     }
     
-    func scanPromptForHandle(message: NSString) {
-
+    func scanPromptForHandle(message: NSString?) -> Bool {
+        let prompt = "\n#What's your name?"
+        if message == nil {
+            return false
+        }
+        let range = message!.rangeOfString(prompt)
+        if range.location != NSNotFound {
+            return true
+        } else {
+            return false
+        }
     }
 }
